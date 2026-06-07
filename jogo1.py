3f9902a2be304a64f3db61837b2ff8eed1d52ebf
Kaw77 <c.wagner.oliveira@gmail.com>
2026-06-07 17:22:28 -0300

    Fazer prints para maior controle

--- jogo1.py
+++ jogo1.py
@@ -1,7 +1,11 @@
 import pygame
 
+
+print('Configurações iniciais')
 pygame.init()
 cria_janela = pygame.display.set_mode(size=(600, 480))
+print('Configurações finais')
 
+print('Começa o loop')
 while True:
     pass
